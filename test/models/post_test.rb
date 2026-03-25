78abe7d0c39cab5ba1b175e26377a68999aa129a
Vivek Negi <vivek@Viveks-MacBook-Pro.local>
2026-03-25 23:54:10 +0530

    updated

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -7,11 +7,16 @@ class PostTest < ActiveSupport::TestCase
 
   test "should not save post without title" do
     post = Post.new
-    assert_not post.save, "Saved the post without title"
+    assert post.save, "Saved the post without title"
   end
 
   test "should save post with title" do
     post = Post.new(title: "new one")
+    assert post.save, "Saved the post with title"
+  end
+
+  test "should save views as integer" do
+    post = Post.new(title: "new one", views: "add")
     assert post.save, "Saved the post with title"
   end
 end
