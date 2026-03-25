5a920c82e9b81d06680904c68c912ad861a36755
Vivek Negi <vivek@Viveks-MacBook-Pro.local>
2026-03-25 23:35:18 +0530

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_25_170447) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_25_180012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,5 +19,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_25_170447) do
     t.datetime "created_at", null: false
     t.string "title"
     t.datetime "updated_at", null: false
+    t.integer "views"
   end
 end
