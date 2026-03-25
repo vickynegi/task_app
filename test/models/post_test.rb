cde877e25ea201e92ff12ee7669d084518008252
Vivek Negi <vivek@Viveks-MacBook-Pro.local>
2026-03-25 23:29:02 +0530

    updated

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -5,12 +5,12 @@ class PostTest < ActiveSupport::TestCase
   #   assert true
   # end
 
-  test 'should not save post without title' do
+  test "should not save post without title" do
     post = Post.new
     assert_not post.save, "Saved the post without title"
   end
 
-  test 'should save post with title' do
+  test "should save post with title" do
     post = Post.new(title: "new one")
     assert post.save, "Saved the post with title"
   end
